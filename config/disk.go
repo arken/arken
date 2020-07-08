4821dc899f1e74ebf9471d9ab41dad6a8247e518
Danilo Hidalgo-Putnam <danilohidalgo@email.arizona.edu>
2020-07-07 19:59:06 -0700

    Add helper functiuons to clean up code
    Also make it so that if the given value is not valid and must be changed, this change is
    reflected in the global config struct. The new value will be inserted in bytes so as to
    avoid floating point values in GB. For example, if the user wants to allocate "max",
    Global.General.PoolSize will be changed to the user's hard drive capacity in bytes,
    in addition to that value being put in GlobalDiskInfo.

--- config/disk.go
+++ config/disk.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+    "fmt"
     "golang.org/x/sys/unix"
     "log"
     "math"
@@ -70,8 +71,8 @@ func (di* DiskInfo) Refresh() {
 //a user must have available is 10 GB.
 //The string must be in the following format and order:
 //  1. A base-10 integer that does not start with 0
-//  2. One of the following: MB, GB, TB. It's case sensitive to avoid bit/byte
-//     confusion. These are mega-, giga-, and terabytes.
+//  2. One of the following: B, KB, MB, GB, or TB. It's case sensitive to avoid
+//     bit/byte confusion.
 //There can be any amount of whitespace before and after either of the elements.
 //  "3000MB", "  10    GB   ", "10 GB" will all work.
 //  "010GB", "10gb", "0xfa5GB" will not work
@@ -79,50 +80,54 @@ func ParsePoolSize(di* DiskInfo) {
     di.Refresh()
     max := Global.General.PoolSize
     defaultSizeB := int64(di.AvailableBytes) - 10000000000 //available - 10GB
-    defaultSizeGB := float64(defaultSizeB) / math.Pow10(9) //for use in strings
+    defaultSizeGB := toGB(uint64(defaultSizeB)) //for use in strings
     if defaultSizeB < 0 { //user has less than 10GB available
         log.Fatal("Not enough free storage on this device")
     }
     var poolSizeB uint64
-    var err error
-    parentRegex := regexp.MustCompile("\\A\\s*[1-9]\\d*\\s*[MGT]B\\s*$")
+    parentRegex := regexp.MustCompile("\\A\\s*[1-9]\\d*\\s*[KMGT]?B\\s*$")
     if strings.EqualFold(max, "max") { //case insensitive comparison
         poolSizeB = di.AvailableBytes
+        Global.General.PoolSize = fmt.Sprintf("%vB", di.AvailableBytes)
     } else if parentRegex.MatchString(max) {
-        bytesStr := regexp.MustCompile("[1-9]\\d*").FindString(max) //extract the number
-        unitStr := regexp.MustCompile("[MGT]B").FindString(max) //extract the unit (GB/MB)
-        poolSizeB, err = strconv.ParseUint(bytesStr, 10, 64) //convert number to uint64
-        if err != nil { //theoretically, the regex should avoid parsing errors.
-            log.Printf("Unable to understand \"%v\" as max pool size," +
-                " using %v GB instead\n", max, defaultSizeGB)
-            poolSizeB = uint64(defaultSizeB)
-        } else {
-            poolSizeB = parseStorageUnit(&unitStr, poolSizeB)
-            if poolSizeB < 1000000000 {
-                log.Fatal("Arken requires an allocation of at least 1 GB")
-            }
-        }
+        poolSizeB = parseWellFormedPoolSize(max)
     } else { //did not match parent regex
         log.Printf("Unable to understand \"%v\" as max pool size," +
-            " attempting to use %v GB instead\n", max, defaultSizeGB)
+            " using %v GB instead\n", max, defaultSizeGB)
         poolSizeB = uint64(defaultSizeB)
+        Global.General.PoolSize = fmt.Sprintf("%vB", defaultSizeB)
     }
     if poolSizeB > di.AvailableBytes {
-        log.Printf("Less than the requested %v GB is/are free on this computer, " +
-            "using %v GB instead\n", float64(poolSizeB) / math.Pow10(9), defaultSizeGB)
+        log.Printf("Less than the requested %v GB are free on this computer, " +
+            "using %v GB instead\n", toGB(poolSizeB), defaultSizeGB)
         poolSizeB = uint64(defaultSizeB)
+        Global.General.PoolSize = fmt.Sprintf("%vB", defaultSizeB)
     }
-    GlobalDiskInfo.PoolSizeBytes = poolSizeB
-    log.Printf("Using %v GB of storage", float64(poolSizeB) / math.Pow10(9))
+    di.PoolSizeBytes = poolSizeB
 }
 
-func parseStorageUnit(unitStr *string, num uint64) uint64 {
-    if *unitStr == "TB" {
-        num *= uint64(math.Pow10(12))
-    } else if *unitStr == "GB" {
-        num *= uint64(math.Pow10(9))
-    } else {
-        num *= uint64(math.Pow10(6))
+func parseWellFormedPoolSize(str string) uint64 {
+    bytesStr := regexp.MustCompile("[1-9]\\d*").FindString(str) //extract the number
+    unitStr := regexp.MustCompile("[KMGT]?B").FindString(str) //extract the unit of storage
+    bytes, _ := strconv.ParseUint(bytesStr, 10, 64) //convert number to uint64
+    switch unitStr {
+    case "TB":
+        bytes *= uint64(math.Pow10(12))
+    case "GB":
+        bytes *= uint64(math.Pow10(9))
+    case "MB":
+        bytes *= uint64(math.Pow10(6))
+    case "KB":
+        bytes *= uint64(math.Pow10(3))
     }
-    return num
+    if bytes < 1000000000 {
+        log.Fatal("Arken requires an allocation of at least 1 GB")
+    }
+    return bytes
+}
+
+//Takes in a uint64 of bytes, return a float64 representing the amount of bytes
+//in gigabytes.
+func toGB(bytes uint64) float64 {
+    return float64(bytes) / math.Pow10(9)
 }
