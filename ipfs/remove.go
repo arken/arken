00c51848d359833e50d7f3af86a585f072b99700
Alec Scott <alecbcs@gmail.com>
2020-08-19 15:16:13 -0700

    Add garbage collection to repository.

--- ipfs/remove.go
+++ ipfs/remove.go
@@ -1,8 +1,11 @@
 package ipfs
 
 import (
+	"context"
+	"fmt"
 	"strings"
 
+	"github.com/ipfs/go-ipfs/gc"
 	"github.com/ipfs/interface-go-ipfs-core/options"
 	icorepath "github.com/ipfs/interface-go-ipfs-core/path"
 )
@@ -15,11 +18,15 @@ func Unpin(hash string) (err error) {
 		input.Recursive = true
 		return nil
 	})
-	if err != nil {
-		if strings.HasPrefix(err.Error(), "not pinned") {
-			return nil
-		}
+	if err != nil && !strings.HasPrefix(err.Error(), "not pinned") {
 		return err
+	}
+	var gcout <-chan gc.Result
+	go func() {
+		gcout = gc.GC(context.Background(), node.Blockstore, node.Repo.Datastore(), node.Pinning, nil)
+	}()
+	for cid := range gcout {
+		fmt.Printf("Garbage Collected: %s\n", cid)
 	}
 	return nil
 }
