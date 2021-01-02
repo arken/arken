a2271b219c7d195aa9f1f48fcd347ee2715973f1
Alec Scott <alec@bcs.sh>
2021-01-02 13:45:07 -0700

    Bump Application Version

--- config/default.go
+++ config/default.go
@@ -16,7 +16,7 @@ func defaultConf() Config {
 			// Configuration version number. If a field is added or changed
 			// in this default, the version must be changed to tell the app
 			// to rebuild the users config files.
-			Version:        "0.2.3",
+			Version:        "0.2.4",
 			PoolSize:       "50 GB",
 			NetworkLimit:   "50 GB",
 			StatsReporting: "OFF",
